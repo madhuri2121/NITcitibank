6c7a98ff8f2aaedd27dee2ad05e5da828f36156f
TL <TL@gmail.com>
2023-10-19 08:52:25 +0530

    secondary push

---
+++ helloworld/src/com/nt/helloworld/Helloworld.java
@@ -0,0 +1,12 @@
+package com.nt.helloworld;
+
+public class Helloworld {
+public void method1() {
+	
+	System.out.println("hello world");
+	
+}
+public static void main(String[] args) {
+	
+}
+}
---
+++ helloworld/src/com/nt/hhh/Mmmm.java
@@ -0,0 +1,9 @@
+package com.nt.hhh;
+
+public class Mmmm {
+	public void method2() {
+		
+		System.out.println("hello world1");
+		
+	}
+}
